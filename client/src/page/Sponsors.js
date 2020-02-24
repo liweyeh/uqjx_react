c10594ba41a867065e35e0fad4501cab77c4d354
Liwei Yeh <l.yeh@uqconnect.edu.au>
2020-02-24 16:32:27 +1000

    temp page for sponsors

--- client/src/page/Sponsors.js
+++ client/src/page/Sponsors.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import BuildIcon from '@material-ui/icons/Build';
 
 const Sponsors = () => {
-  return <div>hi</div>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center'
+      }}
+    >
+      <BuildIcon style={{ fontSize: 100 }} />{' '}
+      <h1>We are still building this part. Sorry for this</h1>
+    </div>
+  );
 };
 
 export default Sponsors;
