41ad6d8dd79981d0c3cc63ff37946daafd7f255a
Liwei Yeh <l.yeh@uqconnect.edu.au>
2020-04-07 20:21:06 +1000

    login FE

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,15 @@ import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ThemeProvider from './theme/ThemeProvider';
 import { CircularProgress } from '@material-ui/core';
-
+// Context
+import AuthState from './context/auth/AuthState';
 // Language Import
 import { IntlProvider } from 'react-intl';
 import messages_ja from './translations/ja.json';
 import messages_en from './translations/en.json';
 const messages = {
   ja: messages_ja,
-  en: messages_en
+  en: messages_en,
 };
 const language = navigator.language.split(/[-_]/)[0] === 'ja' ? 'ja' : 'en';
 
@@ -27,19 +28,21 @@ const App = () => {
   return (
     <IntlProvider locale={language} messages={messages[language]}>
       <ThemeProvider>
-        <Router>
-          <Suspense fallback={<CircularProgress />}>
-            <NavBar />
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/signup' component={Signup} />
-              <Route exact path='/intro' component={Intro} />
-              <Route exact path='/intro/about' component={About} />
-              <Route exact path='/intro/event' component={Events} />
-              <Route exact path='/intro/sponsor' component={Sponsors} />
-            </Switch>
-          </Suspense>
-        </Router>
+        <AuthState>
+          <Router>
+            <Suspense fallback={<CircularProgress />}>
+              <NavBar />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/signup' component={Signup} />
+                <Route exact path='/intro' component={Intro} />
+                <Route exact path='/intro/about' component={About} />
+                <Route exact path='/intro/event' component={Events} />
+                <Route exact path='/intro/sponsor' component={Sponsors} />
+              </Switch>
+            </Suspense>
+          </Router>
+        </AuthState>
       </ThemeProvider>
     </IntlProvider>
   );
--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -1,12 +1,15 @@
 // Dependencies
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
   AppBar,
   Toolbar,
   Typography,
   Button,
-  IconButton
+  IconButton,
+  Modal,
+  Paper,
+  TextField,
 } from '@material-ui/core/';
 import { FormattedMessage } from 'react-intl';
 import { useHistory } from 'react-router-dom';
@@ -15,27 +18,187 @@ import { useHistory } from 'react-router-dom';
 import { ArrowBack, ExpandMore } from '@material-ui/icons';
 import japanFlag from '../../assets/img/Japan_Flag.png';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    height: '10vh'
+    height: '10vh',
   },
   button: {
-    marginRight: theme.spacing(2)
+    marginRight: theme.spacing(2),
   },
   rightSideContent: {
     position: 'absolute',
-    right: '16px'
-  }
+    right: '16px',
+  },
+  form: {
+    width: '20vw',
+    minWidth: '400px',
+    padding: theme.spacing(2),
+    backgroundColor: theme.palette.primary.main,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    marginBottom: '5rem',
+  },
+  formRoot: {
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  formItem: {
+    margin: theme.spacing(2),
+    width: '80%',
+  },
+  textInput: {
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: theme.palette.secondary.main,
+      },
+      '&:hover fieldset': {
+        borderColor: theme.palette.secondary.main,
+      },
+    },
+    '& .MuiInputLabel-root': {
+      color: theme.palette.secondary.main,
+    },
+    'margin': theme.spacing(2),
+    'width': '80%',
+  },
+  submit: {
+    margin: theme.spacing(2),
+    width: '40%',
+  },
 }));
 
-const NavBar = basePath => {
+const NavBar = (basePath) => {
   // hooks
   const classes = useStyles();
   const { goBack, location } = useHistory();
   const { pathname } = location;
-
   const originalPath = '/';
+
+  // States
+  const [open, setOpen] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [societyPassword, setSocietyPassword] = useState('');
+
+  // Methods
+  const handleClick = (origin) => {
+    if (origin === 'register') {
+      setModalContent(origin);
+    } else {
+      setModalContent(origin);
+    }
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setModalContent(null);
+  };
+
+  const handleSubmit = () => {
+    console.log('hi');
+  };
+
+  // Forms
+  const loginForm = (
+    <Paper className={classes.form} elevation={5}>
+      <Typography color='secondary' variant='h4' className={classes.formItem}>
+        <FormattedMessage id='navbar.login' defaultMessage='Staff Login' />
+      </Typography>
+      <TextField
+        label='User'
+        variant='outlined'
+        color='secondary'
+        className={classes.textInput}
+        value={user}
+        autoComplete='no'
+        autoFocus
+        onChange={(e) => setUser(e.target.value)}
+      />
+      <TextField
+        label='Password'
+        variant='outlined'
+        color='secondary'
+        className={classes.textInput}
+        value={password}
+        autoComplete='no'
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button
+        className={classes.submit}
+        color='secondary'
+        variant='outlined'
+        onClick={handleSubmit}
+      >
+        <Typography color='secondary'>
+          <FormattedMessage id='navbar.login' defaultMessage='Login' />
+        </Typography>
+      </Button>
+    </Paper>
+  );
+
+  const registerForm = (
+    <Paper className={classes.form} elevation={5}>
+      <Typography color='secondary' variant='h4' className={classes.formItem}>
+        <FormattedMessage id='navbar.register' defaultMessage='Signup' />
+      </Typography>
+      <TextField
+        label='User'
+        variant='outlined'
+        color='secondary'
+        className={classes.textInput}
+        value={user}
+        autoComplete='no'
+        autoFocus
+        onChange={(e) => setUser(e.target.value)}
+      />
+      <TextField
+        label='Password'
+        variant='outlined'
+        color='secondary'
+        className={classes.textInput}
+        value={password}
+        autoComplete='no'
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <TextField
+        label='Confirm Password'
+        variant='outlined'
+        color='secondary'
+        className={classes.textInput}
+        value={password2}
+        autoComplete='no'
+        onChange={(e) => setPassword2(e.target.value)}
+      />
+      <TextField
+        label='Society Password'
+        variant='outlined'
+        color='secondary'
+        className={classes.textInput}
+        value={societyPassword}
+        autoComplete='no'
+        onChange={(e) => setSocietyPassword(e.target.value)}
+      />
+      <Button
+        className={classes.submit}
+        color='secondary'
+        variant='outlined'
+        onClick={handleSubmit}
+      >
+        <Typography color='secondary'>
+          <FormattedMessage id='navbar.register' defaultMessage='register' />
+        </Typography>
+      </Button>
+    </Paper>
+  );
 
   return (
     <div className={classes.root}>
@@ -62,14 +225,31 @@ const NavBar = basePath => {
               <img src={japanFlag} alt='flag' />
               <ExpandMore />
             </IconButton>
-            <Button color='inherit'>
+            <Button color='inherit' onClick={() => handleClick('login')}>
               <Typography variant='h6' className={classes.title}>
                 <FormattedMessage id='navbar.login' defaultMessage='Login' />
+              </Typography>
+            </Button>
+            <Button color='inherit' onClick={() => handleClick('register')}>
+              <Typography variant='h6' className={classes.title}>
+                <FormattedMessage
+                  id='navbar.register'
+                  defaultMessage='Register'
+                />
               </Typography>
             </Button>
           </div>
         </Toolbar>
       </AppBar>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='simple-modal-title'
+        aria-describedby='simple-modal-description'
+        className={classes.formRoot}
+      >
+        {modalContent === 'register' ? registerForm : loginForm}
+      </Modal>
     </div>
   );
 };
